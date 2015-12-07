c49fa50c439bda0bf6d2333eea6c4f4b2fbabafc
Gbenga <oofhc@mst.edu>
2015-12-07 10:51:20 -0600

    updated and added images

--- cs2300.sql
+++ cs2300.sql
@@ -1,12 +1,4 @@
-CREATE TABLE IF NOT EXISTS `world` (
-	`xpos`	bigint(20), 
-	`ypos`	bigint(20), 
-	`zpos`	bigint(20), 
-	`id`	bigint(20),  
-	PRIMARY KEY (`xpos`, `ypos`, `zpos`) 
-);
-
-INSERT INTO `world` (`xpos`, `ypos`, `zpos`, `id`) VALUES 
+INSERT INTO `BlockInstance` (`xpos`, `ypos`, `zpos`, `objectID`) VALUES 
 (0, 0, 0, 1),
 (0, 1, 0, 1),
 (1, 1, 0, 1),
