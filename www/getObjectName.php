a210d551c2a2ee64abe98cfaf1943b1c4b72d249
Gbenga <oofhc@mst.edu>
2015-12-06 23:57:39 -0600

    fixes for getObjectName

--- www/getObjectName.php
+++ www/getObjectName.php
@@ -4,7 +4,8 @@
 	mysql_connect($mysql_host,$mysql_user,$mysql_password) or die(mysql_error());
   mysql_select_db($mysql_database) or die(mysql_error());
 	
-	$result = mysql_query("SELECT name FROM `Object` WHERE (`id` == $id)");
+	$id = $_GET['id'];
+	$result = mysql_query("SELECT name FROM `Object` WHERE `objectID` = '$id'");
 	
 	$data = array();
 	while($row = mysql_fetch_row($result)) {
