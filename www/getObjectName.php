58cc907afec975eecd65340a353c6d346cc6eb90
Gbenga <oofhc@mst.edu>
2015-12-06 22:37:08 -0600

    Added table to show selection details
    reoriented x and y axis + controls
    added a php script to retrive object names
    put object name into details table

---
+++ www/getObjectName.php
@@ -0,0 +1,15 @@
+<?php
+	include_once "global.php";
+	
+	mysql_connect($mysql_host,$mysql_user,$mysql_password) or die(mysql_error());
+  mysql_select_db($mysql_database) or die(mysql_error());
+	
+	$result = mysql_query("SELECT name FROM `Object` WHERE (`id` == $id)");
+	
+	$data = array();
+	while($row = mysql_fetch_row($result)) {
+		$data[] = $row;
+	}
+	
+	echo json_encode($data);
+?>
